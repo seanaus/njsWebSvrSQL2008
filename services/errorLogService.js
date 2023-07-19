fe9e8af025a7b7bc94b58e47149b58114cca9b34
seanaus <94388567+seanaus@users.noreply.github.com>
2023-07-19 20:20:06 +0100

    Log messages to csv file

--- services/errorLogService.js
+++ services/errorLogService.js
@@ -3,41 +3,17 @@ const path = require('path');
 const configService = require('./configService');
 const logError = (err) => {
 
-    // const code = cleanText(err.code);
-    // const message = cleanText(err.message);
-    // const stack = err.stack;
-    // const dateTime = new Date().toLocaleString('en-GB').replace(',', ':');
-
     const line = `ERROR,${cleanText(err.code)} ${cleanText(err.message)},${getDateTime()} \n`;
     logToFile(configService.errLogDir, configService.errLogFileName, line);
 
 }
 const log = (message) => {
-
-    // const dateTime = new Date().toLocaleString('en-GB').replace(',', ':');
     const line = `INFO,${cleanText(message)},${getDateTime()} \n`;
     logToFile(configService.errLogDir, configService.errLogFileName, line);
 }
-// const logToFile = async (message) => {
-//     const fs = require('fs/promises');
-//     console.log('logToFile');
-//     try {
-//         await fs.writeFile('/Users/seana/ErrorLogFile.txt', message);
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     // fs.writeFile('/errLog.txt', message, err => {
-//     //     if (err) {
-//     //         console.error(err);
-//     //     }
-//     // });
-//     fs.close
-// }
+
 const logToFile = (dir, filename, data) => {
 
-    // console.log(`DIR: ${dir}`);
-    // console.log(`FILENAME: ${filename}`);
-    // console.log(`DATA: ${data}`);
     const file = path.join(__dirname, '../', dir, filename);
 
     return fs.appendFile(file, data, err => {
